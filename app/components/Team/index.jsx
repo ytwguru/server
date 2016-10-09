39a942e93613e176d9d47e095d4dc534da8bbb17
Yomi Toba <ytwguru@gmail.com>
2016-10-09 07:12:37 -0500

    Design team section

--- app/components/Team/index.jsx
+++ app/components/Team/index.jsx
@@ -12,7 +12,7 @@ export default React.createClass({
           <div className="col-md-12">
             <h1>Meet our team</h1>
           </div>
-          <div className="col-md-3">
+          <div className="col-md-6">
             <article className="boxWrapper color1">
               <div><img className="img-responsive" src="./assets/images/team/yomi.gif" alt="" /></div>
               <div className="boxContent2">
@@ -30,12 +30,12 @@ export default React.createClass({
               </div>
             </article>
           </div>
-          <div className="col-md-3">
+          <div className="col-md-6">
             <article className="boxWrapper color1">
               <div><img className="img-responsive" src="./assets/images/team/tanya.png" alt="" /></div>
               <div className="boxContent2">
                 <h3>Tanya Toba</h3>
-                <p>Chief Operating Officer</p>
+                <p>Chief Operational Officer</p>
               </div>
               <div className="boxContent"> <a href="mailto:tanya@ytadvisors.com">tanya@ytadvisors.com</a>
                 <ul className="socialNetwork">
@@ -44,54 +44,6 @@ export default React.createClass({
                   <li><a href="#" className="tips" title="" data-original-title="follow me on Google+"><i className="icon-gplus"></i></a></li>
                   <li><a href="#" className="tips" title="" data-original-title="follow me on Linkedin"><i className="icon-linkedin"></i></a></li>
                   <li><a href="#" className="tips" title="" data-original-title="follow me on Pinterest"><i className="icon-pinterest-circled"></i></a></li>
-                </ul>
-              </div>
-            </article>
-          </div>
-          <div className="col-md-3">
-            <article className="boxWrapper color1">
-              <div><img className="img-responsive" src="./assets/images/team/pic3.jpg" alt="" /></div>
-              <div className="boxContent2">
-                <h3>Alexander Doe</h3>
-                <p>Graphic designer</p>
-              </div>
-              <div className="boxContent"> <a href="mailto:your.email@little-neko.com">a.doe@altea.com</a>
-                <ul className="socialNetwork">
-                  <li><a href="#" className="tips" title="" data-original-title="follow me on Facebook"><i className="icon-facebook"></i></a></li>
-                  <li><a href="#" className="tips" title="" data-original-title="follow me on Twitter"><i className="icon-twitter-bird"></i></a></li>
-                  <li><a href="#" className="tips" title="" data-original-title="follow me on Google+"><i className="icon-gplus"></i></a></li>
-                  <li><a href="#" className="tips" title="" data-original-title="follow me on Linkedin"><i className="icon-linkedin"></i></a></li>
-                  <li><a href="#" className="tips" title="" data-original-title="follow me on Pinterest"><i className="icon-pinterest-circled"></i></a></li>
-                </ul>
-              </div>
-            </article>
-          </div>
-          <div className="col-md-3">
-            <article className="boxWrapper color1">
-              <div><img className="img-responsive" src="./assets/images/team/pic4.jpg" alt="" /></div>
-              <div className="boxContent2">
-                <h3>Pamela Anderson</h3>
-                <p>Programmer</p>
-              </div>
-              <div className="boxContent"> <a href="mailto:your.email@little-neko.com">p.anderson66@altea.com</a>
-                <ul className="socialNetwork">
-                  <li>
-                    <a href="#" className="tips" title="" data-original-title="follow me on Facebook"><i className="icon-facebook"></i></a>
-                  </li>
-                  <li>
-                    <a href="#" className="tips" title="" data-original-title="follow me on Twitter"><i className="icon-twitter-bird"></i></a>
-                  </li>
-                  <li>
-                    <a href="#" className="tips" title="" data-original-title="follow me on Google+"><i className="icon-gplus"></i></a>
-                  </li>
-                  <li>
-                    <a href="#" className="tips" title="" data-original-title="follow me on Linkedin"><i className="icon-linkedin"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#" className="tips" title="" data-original-title="follow me on Pinterest"><i className="icon-pinterest-circled"></i>
-                    </a>
-                  </li>
                 </ul>
               </div>
             </article>
