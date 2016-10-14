afdc2530bc405020b228385bff7f51d64de0c154
Yomi Toba <ytwguru@gmail.com>
2016-10-13 19:23:03 -0500

    Update code

--- app/server/api/quotes.js
+++ app/server/api/quotes.js
@@ -2,11 +2,11 @@ import Express from "express";
 
 var router = Express.Router();
 
-router.get("/", function(req, res){
+router.get("/*", function(req, res){
   res.send('respond with a resource')
 });
 
-router.post("/", function(req, res){
+router.post("/*", function(req, res){
   console.log(req.body);
   res.send({
     success : true
