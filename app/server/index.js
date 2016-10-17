75bfb8f8707adbc349ebdef5d5a2008618419f34
Yomi Toba <ytwguru@gmail.com>
2016-10-17 12:05:14 -0500

    Add compression

--- app/server/index.js
+++ app/server/index.js
@@ -4,12 +4,14 @@ import Express from "express";
 import contacts from "./api/contacts";
 import quotes from "./api/quotes";
 import bodyParser from "body-parser";
+import compression from "compression";
 
 const port = process.env.SITE_PORT || 3000;
 const app = Express();
 
 app.set("view engine", "html");
 app.set("views", "client");
+app.use(compression());
 app.use(Express.static('client'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
