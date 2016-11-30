385e1c6804b7f04cbafa2b6da1bb8baa31bbcc32
Yomi Toba <ytwguru@gmail.com>
2016-11-29 20:56:43 -0600

    Add headers to allow cross origin requests

--- src/server.js
+++ src/server.js
@@ -18,4 +18,18 @@ app.use("/contacts", routes.contacts);
 app.use("/quotes", routes.quotes);
 app.use("/", routes.default);
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+});
+
 app.listen(port);
